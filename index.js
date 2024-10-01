04eb8bf0c43bca9c4ee4d1da8936eb7762b512d6
angel26coder <angelamontes2024@gmail.com>
2024-09-30 21:47:12 -0300

    Homework

---
+++ index.js
@@ -0,0 +1,16 @@
+let myName = "Ángela";
+let myAge = 22;
+let iAm = "filmmaker";
+myAge = 2024 - 2001;
+console.log("Hi, my name is " + myName + ", I'm " + myAge + " years old, and I'm a " + iAm)
+
+let presentation = `Hello, my name is ${myName}, I'm ${myAge} years old, and I'm a ${iAm}`
+console.log(presentation)
+
+
+myName = "Stella";
+myAge = 60;
+iAm = "seller";
+
+herAge = 2024 - 1964;
+console.log("Hi, my grandma is called " + myName + ", she is " + myAge + " years old, and she is a " + iAm)
